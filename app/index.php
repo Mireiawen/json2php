e7317b0e111b4a4ed6a53e128def9b4e8144c89e
Mireiawen Rose <mir@mireiawen.net>
2019-04-09 10:26:11 +0300

    Don't throw JSON error when no data is POST'ed

--- app/index.php
+++ app/index.php
@@ -11,20 +11,20 @@ $error = '';
 if (isset($_POST['json']))
 {
 	$json = $_POST['json'];
-}
-
-try
-{
-	$data = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
-	$generator = new ValueGenerator($data, ValueGenerator::TYPE_ARRAY_SHORT);
-	$generator->setIndentation('  ');
-	$php = $generator->generate();
-}
-catch (\JsonException $e)
-{
-	$error = $smarty -> createTemplate('error.tpl.html');
-	$error -> assign('message', $e -> getMessage());
-	$error = $error -> fetch();
+	
+	try
+	{
+		$data = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+		$generator = new ValueGenerator($data, ValueGenerator::TYPE_ARRAY_SHORT);
+		$generator->setIndentation('  ');
+		$php = $generator->generate();
+	}
+	catch (\JsonException $e)
+	{
+		$error = $smarty -> createTemplate('error.tpl.html');
+		$error -> assign('message', $e -> getMessage());
+		$error = $error -> fetch();
+	}
 }
 
 $tpl -> assign('error', $error);
